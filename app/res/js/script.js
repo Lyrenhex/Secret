96c696bf81f575ed3c1659c17bc11b0fa3914707
Damian Heaton <dh64784@gmail.com>
2017-01-06 18:47:35 +0000

    Add login + signup system, and store keys in the cloud (secret keys protected by account credentials).

--- app/res/js/script.js
+++ app/res/js/script.js
@@ -26,6 +26,9 @@ pgp.initWorker ();
 
 pgp.config.aead_protect = true;
 
+var keys = 1;
+var USER;
+
 // if it doesn't exist, make the keys folder
 fs.mkdir ( 'keys', 0777, function ( error ) {
   if ( error ) {
@@ -43,34 +46,101 @@ electron.ipcRenderer.on ( 'log', function ( event, message ) {
 } );
 
 $(document).ready( function () {
+  showLoad();
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBJD3Yzn8oBmZZvI6QdlwbHDbICQadnohQ",
+    authDomain: "secret-411a9.firebaseapp.com",
+    databaseURL: "https://secret-411a9.firebaseio.com",
+    storageBucket: "secret-411a9.appspot.com",
+    messagingSenderId: "448282861257"
+  };
+  firebase.initializeApp(config);
 
+  var auth = firebase.auth();
+  var database = firebase.database();
+  auth.onAuthStateChanged(function(user){
+    hideLoad();
+    if(user){
+      // signed in -- do some fancy stuff
+      firebase.database().ref(`/${user.uid}/data/seckeys`).on('child_added', function(snapshot) {
+        console.log(snapshot.val());
+        // add the key's card to the keys list thingy
+        keys++;
+      });
+      USER = user;
+    }else{
+      // not signed in -- log in / register
+      showModal ('login-modal');
+    }
+  });
+
+  $("#register").submit(function(e){
+    e.preventDefault();
+    var email = $("#reg-email").val().toString();
+    var pass = $("#reg-password").val().toString();
+
+    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+      console.log(error);
+    });
+
+    hideModal('login-modal');
+  });
+  $("#login").submit(function(e){
+    e.preventDefault();
+    var email = $("#login-email").val().toString();
+    var pass = $("#login-password").val().toString();
+
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+      console.log(error);
+    });
+
+    hideModal('login-modal');
+  });
 } );
 
 function newKeyPair () {
-  document.getElementById ( 'newkey-loader' ).classList.add ( 'display' );
   console.log('generating new keypair');
 
-  var options = {
-    userIds: { name: 'Damian Heaton', email: 'dh64784@gmail.com' },
-    numBits: 4096,
-    passphrase: 'password'
-  }
+  showModal ('newkey-modal');
 
-  pgp.generateKey ( options ).then ( function ( key ) {
-    console.log( key.privateKeyArmored, key.publicKeyArmored );
-    fs.writeFile ( `keys/${options.userIds.name} [${options.userIds.email}].priv.asc`, key.privateKeyArmored, function ( error ) {
-      if ( error ) {
-        console.log ( 'We couldn\'t write the private key', error );
-      }
+  $('#newkey').submit(function(e){
+    e.preventDefault();
+
+    var options = {
+      userIds: { name: $('#newkey-name').val().toString(), email: $('#newkey-email').val().toString() },
+      numBits: parseInt($('#newkey-bits').val()),
+      passphrase: $('#newkey-passphrase').val().toString()
+    }
+
+    hideModal ('newkey-modal');
+
+    showLoad();
+
+    pgp.generateKey ( options ).then ( function ( key ) {
+      console.log( key.privateKeyArmored, key.publicKeyArmored );
+
+      firebase.database().ref(`/${USER.uid}/data/seckeys/${keys}`).set(key.privateKeyArmored);
+      firebase.database().ref(`/pubkeys/${USER.uid}/${keys}`).set(key.publicKeyArmored);
+
+      hideLoad();
     } );
+  });
+}
 
-    fs.writeFile ( `keys/${options.userIds.name} [${options.userIds.email}].pub.asc`, key.publicKeyArmored, function ( error ) {
-      if ( error ) {
-        console.log ( 'We couldn\'t write the public key', error );
-      }
-    } );
-
-    // add the key to the listings
-    document.getElementById ( 'newkey-loader' ).classList.remove ( 'display' );
-  } );
+function showLoad () {
+  document.getElementById('dark-back').classList.add ('display');
+  document.getElementById('load-front').classList.add ('display');
+}
+function hideLoad () {
+  document.getElementById('dark-back').classList.remove ('display');
+  document.getElementById('load-front').classList.remove ('display');
+}
+function showModal (id) {
+  document.getElementById('dark-back').classList.add ('display');
+  document.getElementById(id).classList.add ('display');
+}
+function hideModal (id) {
+  document.getElementById('dark-back').classList.remove ('display');
+  document.getElementById(id).classList.remove ('display');
 }
